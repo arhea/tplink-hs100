3a697a225ba5f57fa557ab79ae89e0d475811562
Alex Rhea <alexrhea@amazon.com>
2019-12-09 12:45:11 -0500

    updating build scripts

--- tplink-hub/build.sh
+++ tplink-hub/build.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-curl https://fr.alpinelinux.org/alpine/v3.5/releases/armhf/alpine-minirootfs-3.5.0-armhf.tar.gz -o rootfs.tar.gz
+curl http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/armhf/alpine-minirootfs-3.10.3-armhf.tar.gz -o rootfs.tar.gz
 
-docker build -f ./build/x86_64/Dockerfile -t arhea/tplink-hs100-hub:latest .
+docker build -f ./build/x86_64/Dockerfile -t arhea/tplink-hs100-hub:amd64 .
 docker build -f ./build/armhf/Dockerfile -t arhea/tplink-hs100-hub:armhf .
 
-docker push arhea/tplink-hs100-hub:latest
-docker push arhea/tplink-hs100-hub:armhf
+docker image push arhea/tplink-hs100-hub:amd64
+docker image push arhea/tplink-hs100-hub:armhf
+
+docker manifest create --amend arhea/tplink-hs100-hub:latest arhea/tplink-hs100-hub:amd64 arhea/tplink-hs100-hub:armhf
+docker manifest push arhea/tplink-hs100-hub:latest
 
 rm -f rootfs.tar.gz
 
